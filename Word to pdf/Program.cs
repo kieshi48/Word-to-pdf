9586b140c45f8c809268277661111782dd894697
kieshi48 <kieshi48@gmail.com>
2023-04-10 15:53:58 +0300

    Добавьте файлы проекта.

---
+++ Word to pdf/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+//using System.Reflection.Metadata;
+using Microsoft.Office.Interop.Word;
+//using static System.Net.Mime.MediaTypeNames;
+
+string folderPath = @"C:\Users\kiesh\Downloads\картинки апк\Калинина — копия";
+string[] files = Directory.GetFiles(folderPath, "*.doc*");
+
+Application word = new Application();
+
+foreach (string file in files)
+{
+    Document doc = word.Documents.Open(file);
+
+    string pdfName = Path.ChangeExtension(file, ".pdf");
+    Console.WriteLine(pdfName);
+    doc.ExportAsFixedFormat(pdfName, WdExportFormat.wdExportFormatPDF);
+
+    doc.Close(false);
+}
+
+word.Quit();
+
+System.Console.WriteLine("All Word documents in the folder have been converted to PDF.");
+System.Console.ReadLine();
